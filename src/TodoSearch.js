f8ba7ad7019327544d6e3f585094d6c25f423042
Giovanny1991 <alexge2009@hotmail.com>
2022-11-15 23:23:24 -0500

    buscador

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function TodoSearch() {
+function TodoSearch({valor,setValor}) {
+
+    const onChangeinput = (eve) => {
+        console.log(eve.target.value);
+        setValor(eve.target.value);
+    }
 
     return (
-       
-            <input className="search" type="text" placeholder="Buscar..."></input>
- 
+
+        <>
+            <input onChange={onChangeinput} className="search" type="text" placeholder="Buscar..." />
+            <p >{valor}</p>
+        </>
     );
 
 }
-export {TodoSearch} ;
+export { TodoSearch };
